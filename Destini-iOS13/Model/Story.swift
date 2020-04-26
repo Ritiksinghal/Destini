4f0de791db24c95428dac0a0e94b49c1495e3fc0
Ritik Singhal <singhalritik00@gmail.com>
2020-04-27 04:39:32 +0530

    Add files via upload

---
+++ Destini-iOS13/Model/Story.swift
@@ -0,0 +1,17 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by Ritik on 08/12/2019.
+//  Copyright © 2019 Ritik. All rights reserved.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
